f8983090af539bbd6fda0946d782fdf942bb91b7
babe25 <babehndr5@gmail.com>
2025-01-18 02:12:52 +0700

    feat(user): Setup user module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(config: ConfigService) {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(logger);
+
+  await app.listen(config.get('PORT'));
 }
-bootstrap();
+const configService = new ConfigService();
+bootstrap(configService);
